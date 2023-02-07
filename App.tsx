81f611f6e572bb26e4031bae8c31614e99bfc128
Onur Vatansever <60274419+onurvatansever6@users.noreply.github.com>
2023-02-07 11:17:14 +0300

    listen, message a geçildi emulatör gateway çalışıy

--- App.tsx
+++ App.tsx
@@ -5,11 +5,12 @@
  * @format
  */
 
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import dgram from 'react-native-udp';
 // import startUDPReceive from './startUDP';
-import DocumentPicker, { DocumentPickerResponse, DirectoryPickerResponse } from 'react-native-document-picker';
-import startUDP from './startUDP';
+// import DocumentPicker, { DocumentPickerResponse, DirectoryPickerResponse } from 'react-native-document-picker';
+// import startUDP from './startUDP';
 
 
 const App: React.FC = () => {
@@ -17,7 +18,6 @@ const App: React.FC = () => {
   const [pcIP, setPcIP] = useState(''); 
   const [ButtonsDeneme, setScanButtons] = useState<ReactNode[]>([]);
   const [scanInProgress, setScanInProgress] = useState(false);
-  const [docResult, setResult] = React.useState<Array<DocumentPickerResponse>>();
   const [allDevices, setAllDevices] = useState<Array<{ pcName: string, pcIp: string }>>([]);
   const [displayText, setDisplayText] = useState('');
   const [displayTextSocket, setDisplayTextSocket] = useState('');
@@ -34,71 +34,98 @@ const App: React.FC = () => {
 
   // };
 
-  const handleScan = async () => {
-    setScanInProgress(true);
-    setDisplayText("butona basıldı");
-    setDisplayTextSocket("socket açıldı 8090a bağlı");
+  // const handleScan = async () => {
+  //   setScanInProgress(true);
+  //   setDisplayText("butona basıldı");
+  //   setDisplayTextSocket("socket açıldı 8090a bağlı");
 
-    const receivedMessage = await startUDP();
-    const { pcName, pcIp } = receivedMessage;
-    setAllDevices(receivedMessage);
-    setDisplayTextData("veriler geldi")
-    setPcName(pcName);
-    setPcIP(pcIp);
-    setScanInProgress(false);
-    setScanButtons(allDevices.map(device => (
-      <Button title={`Connect to ${device.pcName}`} onPress={() => console.log(`Connecting to ${device.pcName} at ${device.pcIp}`)} />
-    )));
-    setDisplayTextButtons("butonlar yaratıldı");
-  };
+  //   const receivedMessage = await startUDP();
+  //   const { pcName, pcIp } = receivedMessage;
+  //   setAllDevices(receivedMessage);
+  //   setDisplayTextData("veriler geldi")
+  //   setPcName(pcName);
+  //   setPcIP(pcIp);
+  //   setScanInProgress(false);
+  //   setScanButtons(allDevices.map(device => (
+  //     <Button title={`Connect to ${device.pcName}`} onPress={() => console.log(`Connecting to ${device.pcName} at ${device.pcIp}`)} />
+  //   )));
+  //   setDisplayTextButtons("butonlar yaratıldı");
+  // };
 
-  const openDocumentFile = async () => {
-    try{
-      const res = await DocumentPicker.pick({
-        type: [DocumentPicker.types.allFiles]
-      })
-      setResult(res);
-    }
-    catch (err) {
-      if (DocumentPicker.isCancel(err)){
+  // const openDocumentFile = async () => {
+  //   try{
+  //     const res = await DocumentPicker.pick({
+  //       type: [DocumentPicker.types.allFiles]
+  //     })
+  //     setResult(res);
+  //   }
+  //   catch (err) {
+  //     if (DocumentPicker.isCancel(err)){
         
-      } else {
-        throw err; 
-      }
-    }
-  }; 
+  //     } else {
+  //       throw err; 
+  //     }
+  //   }
+  // }; 
+  const remotePort = 5000;
+  const remoteHost = '255.255.255.255';
+  const socket = dgram.createSocket('udp4')
+  socket.bind(8081)
+  
+  socket.once('listening', function () {
+    socket.send('Hello World!', undefined, undefined, remotePort, remoteHost, function (err) {
+      if (err) throw err
+
+      console.log('Message sent!')
+    })
+  })
+
+  socket.on('message', function (msg, rinfo) {
+    let [pcName, pcIp] = msg.toString().split(":");
+    console.log('Message received', pcName, pcIp)
+    setPcIP(pcIp);
+    setPcName(pcName);
+
+  })
 
   return (
     <View style={styles.container}>
-      <View >
-        <Button onPress={handleScan} title="deneme"></Button>
-        <Text>-------------------------------------------</Text>
-        <Text>{displayText}</Text>
-        <Text>{displayTextSocket}</Text>
-        <Text>{displayTextData}</Text>
-        <Text>{displayTextButtons}</Text>
-        <View>
-          {ButtonsDeneme.map((button, index) => (
-            <View key={index}>{button}</View>
-          ))}
-        </View>
-        <Text>PC NAME ya da IP aldiysam benim altimda gözükür</Text>
-        <Text>{pcName}{pcIP}</Text>
-      </View>
-      
-      {scanInProgress ? (
-        <View style={[styles.scanningIndicator, { backgroundColor: "red" }]} />
-      ) : (
-        pcName && pcIP ? (
-          <View style={[styles.scanningIndicator, { backgroundColor: "green" }]} />
-        ) : (
-          <View style={[styles.scanningIndicator, { backgroundColor: "red" }]} />
-        )
-      )}
-      <Text style={styles.connectionText}>
-        {pcName && pcIP ? "Connected" : "Disconnected"}
-      </Text>
+      <Text> Deneme </Text>
+      <Text> PC name ve İP gelirse bu yazının altında gözükücek</Text>
+      <Text> {pcName} {pcIP} </Text>
     </View>
+    
+    
+    // <View style={styles.container}>
+    //   <View >
+    //     <Button onPress={handleScan} title="deneme"></Button>
+    //     <Text>-------------------------------------------</Text>
+    //     <Text>{displayText}</Text>
+    //     <Text>{displayTextSocket}</Text>
+    //     <Text>{displayTextData}</Text>
+    //     <Text>{displayTextButtons}</Text>
+    //     <View>
+    //       {ButtonsDeneme.map((button, index) => (
+    //         <View key={index}>{button}</View>
+    //       ))}
+    //     </View>
+    //     <Text>PC NAME ya da IP aldiysam benim altimda gözükür</Text>
+    //     <Text>{pcName}{pcIP}</Text>
+    //   </View>
+      
+    //   {scanInProgress ? (
+    //     <View style={[styles.scanningIndicator, { backgroundColor: "red" }]} />
+    //   ) : (
+    //     pcName && pcIP ? (
+    //       <View style={[styles.scanningIndicator, { backgroundColor: "green" }]} />
+    //     ) : (
+    //       <View style={[styles.scanningIndicator, { backgroundColor: "red" }]} />
+    //     )
+    //   )}
+    //   <Text style={styles.connectionText}>
+    //     {pcName && pcIP ? "Connected" : "Disconnected"}
+    //   </Text>
+    // </View>
   );
   
 };
--- startUDP.js
+++ startUDP.js
@@ -4,7 +4,18 @@ import udp from 'react-native-udp';
 export default function startUDP() {
   return new Promise((resolve) => {
     const socket = udp.createSocket('udp4');
-    socket.bind(8090, "0.0.0.0");
+
+    socket.bind(8090, '0.0.0.0', function () {
+      console.log('bound');
+    });
+
+    socket.once('listening', function() {
+      console.log('listening');
+    })
+
+    socket.on('error', (err) => {
+      console.log(`server error:\n${err.stack}`);
+    });
 
     socket.on('message', (msg, rinfo) => {
       const [pcName, pcIp] = msg.toString().split(":");
