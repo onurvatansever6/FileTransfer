39594d997f4cb18633f44b6091be16609f5b43db
Onur Vatansever <60274419+onurvatansever6@users.noreply.github.com>
2023-02-07 15:47:57 +0300

    button creation added

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
   const [pcIP, setPcIP] = useState(''); 
   const [ButtonsDeneme, setScanButtons] = useState<ReactNode[]>([]);
   const [scanInProgress, setScanInProgress] = useState(false);
-  const [allDevices, setAllDevices] = useState<Array<{ pcName: string, pcIp: string }>>([]);
+  const [allDevices, setAllDevices] = useState<Array<{ pcName: any, pcIp: any}>>([]);
   const [displayText, setDisplayText] = useState('');
   const [displayTextSocket, setDisplayTextSocket] = useState('');
   const [displayTextData, setDisplayTextData] = useState('');
@@ -68,7 +68,7 @@ const App: React.FC = () => {
   //   }
   // }; 
   const remotePort = 5000;
-  const remoteHost = '255.255.255.255';
+  const remoteHost = '10.0.2.2';
   const socket = dgram.createSocket('udp4')
   socket.bind(8081)
   
@@ -80,21 +80,38 @@ const App: React.FC = () => {
     })
   })
 
-  socket.on('message', function (msg, rinfo) {
-    let [pcName, pcIp] = msg.toString().split(":");
-    console.log('Message received', pcName, pcIp)
-    setPcIP(pcIp);
-    setPcName(pcName);
-
-  })
+  const handleScan = () => {
+    socket.on('message', function (msg, rinfo) {
+      let [pcName, pcIp] = msg.toString().split(":");
+      console.log('Message received', pcName, pcIp);
+      let connectedDevice = ({ pcName, pcIp });
+      const addDevice = (connectedDevice: { pcName: any, pcIp: any}) => {
+        setAllDevices([...allDevices, connectedDevice]);
+      };
+      addDevice(connectedDevice);
+      setScanButtons(allDevices.map(device => (
+            <Button title={`Connect to ${device.pcName}`} onPress={() => console.log(`Connecting to ${device.pcName} at ${device.pcIp}`)} />
+          )));
+      setPcIP(pcIp);
+      setPcName(pcName);
+  
+    })
+  }
+  
 
   return (
     <View style={styles.container}>
       <Text> Deneme </Text>
       <Text> PC name ve İP gelirse bu yazının altında gözükücek</Text>
       <Text> {pcName} {pcIP} </Text>
+      <Button onPress={handleScan} title="deneme"></Button>
+      <View>
+        {ButtonsDeneme.map((button, index) => (
+          <View key={index}>{button}</View>
+        ))}
+      </View>
     </View>
-    
+
     
     // <View style={styles.container}>
     //   <View >
