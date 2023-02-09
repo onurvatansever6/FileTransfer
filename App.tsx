aa66495629d89dd24a4fff5bd28919de87570e47
Onur Vatansever <60274419+onurvatansever6@users.noreply.github.com>
2023-02-09 13:10:30 +0300

    front end changes

--- App.tsx
+++ App.tsx
@@ -6,11 +6,13 @@
  */
 
 import React, { ReactNode, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Button } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Button, Dimensions } from 'react-native';
 import dgram from 'react-native-udp';
 // import startUDPReceive from './startUDP';
 // import DocumentPicker, { DocumentPickerResponse, DirectoryPickerResponse } from 'react-native-document-picker';
 // import startUDP from './startUDP';
+
+const percent5 = Dimensions.get('window').width / 20
 
 
 const App: React.FC = () => {
@@ -24,33 +26,6 @@ const App: React.FC = () => {
   const [displayTextData, setDisplayTextData] = useState('');
   const [displayTextButtons, setDisplayTextButtons] = useState('');
 
-  // const handleScan = () => {
-  //   setScanInProgress(true);
-  //   startUDPReceive(setAllDevices);
-  //   setScanInProgress(false);
-  //   setScanButtons(allDevices.map(device => (
-  //     <Button title={`Connect to ${device.pcName}`} onPress={() => console.log(`Connecting to ${device.pcName} at ${device.pcIp}`)} />
-  //   )));
-
-  // };
-
-  // const handleScan = async () => {
-  //   setScanInProgress(true);
-  //   setDisplayText("butona basıldı");
-  //   setDisplayTextSocket("socket açıldı 8090a bağlı");
-
-  //   const receivedMessage = await startUDP();
-  //   const { pcName, pcIp } = receivedMessage;
-  //   setAllDevices(receivedMessage);
-  //   setDisplayTextData("veriler geldi")
-  //   setPcName(pcName);
-  //   setPcIP(pcIp);
-  //   setScanInProgress(false);
-  //   setScanButtons(allDevices.map(device => (
-  //     <Button title={`Connect to ${device.pcName}`} onPress={() => console.log(`Connecting to ${device.pcName} at ${device.pcIp}`)} />
-  //   )));
-  //   setDisplayTextButtons("butonlar yaratıldı");
-  // };
 
   // const openDocumentFile = async () => {
   //   try{
@@ -67,22 +42,37 @@ const App: React.FC = () => {
   //     }
   //   }
   // }; 
+
+  
   const remotePort = 5000;
   const remoteHost = '10.0.2.2';
   const socket = dgram.createSocket('udp4')
   socket.bind(8081)
   
-  socket.once('listening', function () {
-    socket.send('Hello World!', undefined, undefined, remotePort, remoteHost, function (err) {
+  // socket.once('listening', function () {
+  //   socket.send('WHO', undefined, undefined, remotePort, remoteHost, function (err) {
+  //     if (err) throw err
+
+  //     console.log('Message sent!')
+  //   })
+  // })
+
+  socket.on('message', function (msg,rinfo){
+    console.log(msg.toString());
+  })
+
+  const sendMessage = () => {
+    socket.send('WHO', undefined, undefined, remotePort, remoteHost, function (err) {
       if (err) throw err
 
       console.log('Message sent!')
     })
-  })
-
+  }
+  
   const handleScan = () => {
     socket.on('message', function (msg, rinfo) {
       let [pcName, pcIp] = msg.toString().split(":");
+      console.log('rinfo: ', rinfo);
       console.log('Message received', pcName, pcIp);
       let connectedDevice = ({ pcName, pcIp });
       const addDevice = (connectedDevice: { pcName: any, pcIp: any}) => {
@@ -94,23 +84,40 @@ const App: React.FC = () => {
           )));
       setPcIP(pcIp);
       setPcName(pcName);
-  
-    })
+
+      setTimeout(() => {
+        socket.close();
+        console.log('socket closed after 10 sec')
+      }, 10000);
+    });
   }
+
   
 
   return (
-    <View style={styles.container}>
-      <Text> Deneme </Text>
-      <Text> PC name ve İP gelirse bu yazının altında gözükücek</Text>
-      <Text> {pcName} {pcIP} </Text>
-      <Button onPress={handleScan} title="deneme"></Button>
-      <View>
-        {ButtonsDeneme.map((button, index) => (
-          <View key={index}>{button}</View>
-        ))}
+    <>
+      <View style={{ backgroundColor: '#1C1321', flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: percent5 }}>
+        <Text style={{ backgroundColor: '#F6F8FA', width: 3 * percent5, textAlign: 'center', borderRadius: 0.2 * percent5 }}>HOME</Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Text style={{ marginRight: percent5 / 2 , color: '#F6F8FA' }}>Disconnected</Text>
+          <Text style={{ backgroundColor: '#C85348', width: 1.8 * percent5, height: 1.8 * percent5, borderRadius: 0.9 * percent5 }}></Text>
+        </View>
       </View>
-    </View>
+
+
+      <View style={{ backgroundColor: '#E0ECEA', flex: 9 , justifyContent: 'center', alignContent: 'center' }}>
+        <Button title="send message" onPress={sendMessage}></Button>
+        <Button onPress={handleScan} title="Scan"></Button>
+        <View>
+          {ButtonsDeneme.map((button, index) => (
+            <View key={index}>{button}</View>
+          ))}
+        </View>
+      </View>
+    </>
+    
+
+    
 
     
     // <View style={styles.container}>
@@ -146,47 +153,5 @@ const App: React.FC = () => {
   );
   
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  scanButton: {
-    backgroundColor: 'blue',
-    padding: 10,
-    borderRadius: 5,
-  },
-  scanButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  scanningIndicator: {
-    backgroundColor: 'red',
-    width: 10,
-    height: 10,
-    borderRadius: 5,
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
-  resultContainer: {
-    padding: 10,
-  },
-  resultText: {
-    fontWeight: 'bold',
-  },
-  connectionText: {
-    position: 'absolute',
-    top: 5,
-    right: 25,
-  },
-  denemeButton: {
-    position: 'absolute',
-    top: 5,
-    right: 40,
-  }
-});
 
 export default App;
